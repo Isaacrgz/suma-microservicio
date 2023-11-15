311886a909244531d0ff3ff21af81a4817c76d91
Isaacrgz <isaacrgz97@gmail.com>
2023-11-14 21:30:31 -0600

    First commit to github

---
+++ src/main/java/beans/ServicioSumarWS.java
@@ -0,0 +1,11 @@
+package beans;
+
+import jakarta.jws.WebMethod;
+import jakarta.jws.WebService;
+
+@WebService
+public interface ServicioSumarWS {
+    
+    @WebMethod
+    public int sumar (int a, int b);
+}
